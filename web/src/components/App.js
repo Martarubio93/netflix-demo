fdbbf75071782f02ae3fb28ba00a7bb5b4ec9c58
Martarubio93 <you@example.com>
2022-03-17 13:31:59 +0000

    server and web

---
+++ web/src/components/App.js
@@ -0,0 +1,15 @@
+import "../styles/App.css";
+import SignIn from "./SignIn";
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Netflix</h1>
+      </header>
+      <SignIn/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/SignIn.js
@@ -0,0 +1,39 @@
+const SignIn = () => {
+    return(
+        <main>
+        <div>
+          <h2>Sign in</h2>
+          <form>
+            <label className="" htmlFor="">
+              Email or phone number
+            </label>
+            <input
+              className=""
+              id=""
+              type="text"
+              placeholder=""
+              title=""
+              required
+            />
+            <label className="" htmlFor="">
+              Password
+            </label>
+            <input
+              className=""
+              id=""
+              type="text"
+              placeholder=""
+              title=""
+              required
+            />
+            <div className="">
+              <input className="" type="submit" value="Sign in " />
+            </div>
+          </form>
+        </div>
+      </main>
+    )
+}
+
+
+export default SignIn
