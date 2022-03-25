d17e1fb199cb93e4e07ff818877b187a2ce605fb
Martarubio93 <you@example.com>
2022-03-25 17:00:02 +0000

    publish-react

--- web/src/components/FilmList.js
+++ web/src/components/FilmList.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "../styles/layout/FilmListHome.scss";
 
 const FilmList = (props) => {
@@ -21,30 +20,6 @@ const FilmList = (props) => {
           </li>
         );
       });
-=======
-import '../styles/layout/FilmListHome.scss'
-import {Link} from 'react-router-dom'
-
-const FilmList = (props) => {
-  const renderList = () => {
-    return (
-      props.filmsFromApi
-         .filter((film )=> {
-           return film.name.toLowerCase().includes(props.searchEngine.toLowerCase())
-    })
-        .map((film, index) => {
-          
-          return (
-            <Link className="detailedLink">
-            <li key={index} id={film.id} className="filmListContainer__item">
-              <h3 className="filmListContainer__item--title">{film.name}</h3>
-              <img className="filmListContainer__item--img"src={film.image} alt="film"></img>
-            </li>
-            </Link>
-          );
-        })
-    );
->>>>>>> b91b67ac24384cd881e85df8e51b7f19a2f1a702
   };
 
   return (
--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -6,12 +6,8 @@ import martflix from '../images/logo.svg'
 const Header = () => {
   return (
     <header>
-<<<<<<< HEAD
-      
-=======
       <img className="martflix"src={martflix} alt="icon"/>
 
->>>>>>> b91b67ac24384cd881e85df8e51b7f19a2f1a702
     </header>
   );
 };
